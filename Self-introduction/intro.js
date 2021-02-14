4cabe6be016af4f4327c1539474fddba46774c5c
kaine-42 <kaine04021999@gmail.com>
2021-02-14 11:36:35 +0900

    今までの分

---
+++ Self-introduction/intro.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+
+button1.onclick = function () {
+  alert("大正解！これから是非仲良くしてね！")
+}
+button2.onclick = function () {
+  alert("He is Saurav Thapa Shrestha !!!!")
+}
